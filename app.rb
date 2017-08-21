7279bce66bc4aed683bf147b11bbb5e2733f86fc
ScStew <scottmstewart2@gmail.com>
2017-08-21 13:54:04 -0400

    added checkbox's for toppings

--- app.rb
+++ app.rb
@@ -12,3 +12,11 @@ end
 get "/size" do
 	erb :size, locals:[lname:session[:lname]]
 end
+
+post "/size" do
+	size = params[:size]
+	crust = params[:crust]
+	meats = params[:meats]
+	veggies = params[:veggies]
+	special = params[:special]
+end
