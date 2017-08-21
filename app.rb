2f10aed2586c3a19e06592859da143b88e585b11
ScStew <scottmstewart2@gmail.com>
2017-08-21 11:56:11 -0400

    added name page and getting ready here

---
+++ app.rb
@@ -0,0 +1,11 @@
+require "sinatra"
+enable "sessions"
+get "/" do
+	erb: names
+end
+
+post "/names" do
+	session[:fname] = params[:fname]
+	session[:lname] = params[:lname]
+ 	"work in progress"
+end
