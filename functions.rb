68e67cf8ee2b8a2e339420337320ed1ee9c73347
ScStew <scottmstewart2@gmail.com>
2017-08-25 13:16:20 -0400

    updated confirm page to iterate hash and show price of each item

--- functions.rb
+++ functions.rb
@@ -2,7 +2,7 @@ def seperator(arr)
 	final_arr = []
 	arr.each do |x|
 		new_arr = x.split(",")
-		new_hash = {new_arr[0] => new_arr[1].to_f}
+		new_hash = {new_arr[0] => new_arr[1]}
 		final_arr << new_hash
 	end
 	p final_arr
@@ -11,6 +11,6 @@ end
 
 def single_seperator(arr)
 	x = arr.split(",")
-	new_hash = {x[0] => x[1].to_f}
+	new_hash = {x[0] => x[1]}
 	new_hash
 end
